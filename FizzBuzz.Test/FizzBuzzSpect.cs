f80ba9d6c3b37f5b41949195210e13ec978e4d0e
kai.yan <kai.yan@thoughtworks.com>
2019-05-08 11:12:07 +0800

    [qingqing && yankai] init project

---
+++ FizzBuzz.Test/FizzBuzzSpect.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Test
+{
+    public class FizzBuzzSpect
+    {
+        [Fact]
+        public void shoud_return_xxxx()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+        }
+    }
+}
